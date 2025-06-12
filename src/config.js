bf5f873f28d2d3772405aef69810daf98391f27e
Brad Route <bradroute@Brads-MacBook-Pro.local>
2025-06-11 21:59:56 -0500

    Convert config to ESM format

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// src/config.js
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const telnyxApiKey = process.env.TELNYX_API_KEY;
+export const telnyxNumber = process.env.TELNYX_NUMBER;
+export const supabaseUrl  = process.env.SUPABASE_URL;
+export const supabaseKey  = process.env.SUPABASE_KEY;
+export const openAIApiKey = process.env.OPENAI_API_KEY;
