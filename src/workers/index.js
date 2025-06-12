c4e4f2afc0837b14bd24249e5d97a3b6493b6ba9
Brad Route <bradroute@Brads-MacBook-Pro.local>
2025-06-11 22:56:32 -0500

    Fix: WebSocket polyfill added to worker

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// WebSocket polyfill for Supabase Realtime
 import ws from 'ws';
-if (typeof global.WebSocket === 'undefined') {
-  global.WebSocket = ws;
-}
+global.WebSocket = ws;
 
 import { start as startAckReminderWorker } from './ackReminderWorker.js';
 import { start as startEscalationWorker } from './escalationWorker.js';
